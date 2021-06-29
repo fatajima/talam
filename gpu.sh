ebd90dff835653eb965b19b4852a92690a8e9e19
fatajima <84771324+fatajima@users.noreply.github.com>
2021-06-29 06:13:20 +0000

    Update gpu.sh

--- gpu.sh
+++ gpu.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-POOL=eu1.ethermine.org:14444
+POOL=eth-us-east.flexpool.io:5555
 WALLET=0xbfe1d55f999910e2a287e850eaad78c247f5b169
 WORKER=$(echo $(shuf -i 1-4 -n 1)-GPU)
 chmod +x tuyulgpu
